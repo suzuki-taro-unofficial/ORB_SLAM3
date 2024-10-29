d223856743202317d72c60a418425a987c5d196f
pogyomo <pogyomo@gmail.com>
2024-10-29 21:04:22 +0900

    fix: Dockerfileが変更されたときにビルドしたいので条件分岐を削除
    docker buildは変更がなければキャッシュによって一瞬で終わるから
    こちら側でなにかする必要はなかったかも

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,9 +2,7 @@
 
 readonly image_name="orb-slam3-dev"
 
-if [[ -z "$(sudo docker images -q $image_name 2> /dev/null)" ]]; then
-    sudo docker build . -t $image_name
-fi
+sudo docker build . -t $image_name
 
 sudo docker run --rm -it \
     -v .:/ORB_SLAM3 \
