2214d181027610e98ae84591e7c39b5759cad766
pogyomo <pogyomo@gmail.com>
2024-10-30 15:57:21 +0900

    feat: docker composeを使ってdockerを起動するようにした

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,8 +2,4 @@
 
 readonly image_name="orb-slam3-dev"
 
-sudo docker build . -t $image_name
-
-sudo docker run --rm -it \
-    -v .:/ORB_SLAM3 \
-    $image_name
+sudo docker compose run --rm -it dev
