ab07c11a09a3ab4524594369d13458b0ed4ff441
pogyomo <pogyomo@gmail.com>
2024-11-01 15:15:29 +0900

    fix: output error message into stderr instead of stdout (#22)

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -f "/.dockerenv" ]; then
-    echo "cannot launch docker inside docker" 2>&1
+    echo "cannot launch docker inside docker" 1>&2
 else
     sudo docker compose run --build --rm -it dev
 fi
